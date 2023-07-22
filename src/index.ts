66a95b5b25fda3e76b5174db58dc7100f5f4e299
Vitalii Zinoviev <vitaliczinoviev@gmail.com>
2023-07-22 13:35:19 +0300

    Added read program from file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
+import { readFileSync } from 'fs'
 import Lexer from 'Lexer'
 import Parser from 'Parser'
 
-const program = `
-log 'hello'
-log 'world'
-log 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 - 55 
-`
+const program = readFileSync('log.my-js', 'utf8')
 
 const lexer = new Lexer(program)
 const tokens = lexer.tokenize()
