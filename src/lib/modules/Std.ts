71da60c3c630eb0986d29a506b85a7f276b4ff6b
Vitalii Zinoviev <vitaliczinoviev@gmail.com>
2023-08-02 16:12:48 +0300

    Added string func

--- src/lib/modules/Std.ts
+++ src/lib/modules/Std.ts
@@ -14,29 +14,146 @@ import ArrayValue from '@lib/ArrayValue'
 import { ArgumentsMismatchException, TypeException } from 'exceptions/ArgumentsMismatchException'
 import FunctionValue from '@lib/FunctionValue'
 import BooleanValue from '@lib/BooleanValue'
+import StringValue from '@lib/StringValue'
+import StdSort from './functions/StdSort'
 
-export class StdSort implements Function {
-  public execute(...args: IValue[]): IValue {
+class StdSprintf implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length < 1) throw new ArgumentsMismatchException('At least one argument expected')
+
+    const format = args[0].asString()
+    const values: (number | string)[] = new Array(args.length - 1)
+    for (let i = 1; i < args.length; i++) {
+      values[i - 1] = args[i] instanceof NumberValue ? args[i].asNumber() : args[i].asString()
+    }
+    return new StringValue('')
+    // return new StringValue(String.format(format, values))
+  }
+}
+
+class StdSplit implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length < 2 || args.length > 3) throw new ArgumentsMismatchException('Two or three arguments expected')
+
+    const input = args[0].asString()
+    const regex = args[1].asString()
+    const limit = args.length == 3 ? args[2].asNumber() : 0
+
+    const parts = input.split(regex)
+    const result = new ArrayValue(parts.length)
+    for (let i = 0; i < parts.length; i++) result.set(i, new StringValue(parts[i]))
+    return result
+  }
+}
+
+class StdJoin implements Function {
+  execute(...args: IValue[]): IValue {
     if (args.length < 1) throw new ArgumentsMismatchException('At least one argument expected')
     if (!(args[0] instanceof ArrayValue)) throw new TypeException('Array expected in first argument')
 
-    const elements: IValue[] = args[0].getCopyElements()
+    const array = args[0]
     switch (args.length) {
       case 1:
-        elements.sort()
-        break
+        return this.join(array, '', '', '')
       case 2:
-        if (!(args[1] instanceof FunctionValue)) throw new TypeException('Function expected in second argument')
-        const comparator = args[1].getValue()
-        elements.sort((a, b) => {
-          return comparator.execute(a, b).asNumber()
-        })
-        break
+        return this.join(array, args[1].asString(), '', '')
+      case 3:
+        return this.join(array, args[1].asString(), args[2].asString(), args[2].asString())
+      case 4:
+        return this.join(array, args[1].asString(), args[2].asString(), args[3].asString())
       default:
         throw new ArgumentsMismatchException('Wrong number of arguments')
     }
+  }
 
-    return new ArrayValue(elements)
+  private join(array: ArrayValue, delimiter: string, prefix: string, suffix: string) {
+    const sb: string[] = []
+    for (const value of array) {
+      if (sb.length) sb.push(delimiter)
+      else sb.push(prefix)
+      sb.push(value.asString())
+    }
+    sb.push(suffix)
+    return new StringValue(sb.toString())
+  }
+}
+
+export class StdIndexOf implements Function {
+  public execute(...args: IValue[]): IValue {
+    if (args.length < 2 || args.length > 3) throw new ArgumentsMismatchException('Two or three arguments expected')
+
+    const input = args[0].asString()
+    const what = args[1].asString()
+    const index = args.length == 3 ? args[2].asNumber() : 0
+
+    return new NumberValue(input.indexOf(what, index))
+  }
+}
+
+export class StdLastIndexOf implements Function {
+  public execute(...args: IValue[]): IValue {
+    if (args.length < 2 || args.length > 3) throw new ArgumentsMismatchException('Two or three arguments expected')
+
+    const input = args[0].asString()
+    const what = args[1].asString()
+    const index = args.length == 3 ? args[2].asNumber() : 0
+
+    return new NumberValue(input.lastIndexOf(what, index))
+  }
+}
+
+export class StdReplace implements Function {
+  public execute(...args: IValue[]): IValue {
+    if (args.length !== 3) throw new ArgumentsMismatchException('Three arguments expected')
+
+    const input = args[0].asString()
+    const regex = args[1].asString()
+    const replacement = args[2].asString()
+
+    return new StringValue(input.replace(regex, replacement))
+  }
+}
+
+export class StdCharAt implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length !== 2) throw new ArgumentsMismatchException('Two arguments expected')
+
+    const input = args[0].asString()
+    const index = args[1].asNumber()
+
+    return new StringValue(input.charAt(index))
+  }
+}
+
+export class stdToChar implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length !== 1) throw new ArgumentsMismatchException('One argument expected')
+
+    return new StringValue(String.fromCharCode(args[0].asNumber()))
+  }
+}
+
+export class stdToLowercase implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length !== 1) throw new ArgumentsMismatchException('One argument expected')
+
+    return new StringValue(args[0].asString().toLowerCase())
+  }
+}
+
+export class stdToUppercase implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length !== 1) throw new ArgumentsMismatchException('One argument expected')
+
+    return new StringValue(args[0].asString().toUpperCase())
+  }
+}
+
+export class stdTrim implements Function {
+  execute(...args: IValue[]): IValue {
+    if (args.length !== 1) throw new ArgumentsMismatchException('One argument expected')
+
+    return new StringValue(args[0].asString().trim())
   }
 }
 
@@ -51,5 +168,21 @@ export default class Std implements IModule {
     Functions.set('nextFrame', new StdNextFrame())
     Functions.set('thread', new StdThread())
     Functions.set('sort', new StdSort())
+
+    // String
+    Functions.set('sprintf', new StdSprintf())
+    Functions.set('split', new StdSplit())
+    Functions.set('join', new StdJoin())
+    Functions.set('indexOf', new StdIndexOf())
+    Functions.set('lastIndexOf', new StdLastIndexOf())
+    Functions.set('charAt', new StdCharAt())
+    Functions.set('toChar', new stdToChar())
+    // Functions.set('substring', new std_substring())
+    Functions.set('toLowerCase', new stdToLowercase())
+    Functions.set('toUpperCase', new stdToUppercase())
+    Functions.set('trim', new stdTrim())
+    Functions.set('replace', new StdReplace())
+    // Functions.set('replaceAll', new StdReplaceAll())
+    // Functions.set('replaceFirst', new std_replacefirst())
   }
 }
---
+++ src/lib/modules/functions/StdSort.ts
@@ -0,0 +1,30 @@
+import ArrayValue from '@lib/ArrayValue'
+import FunctionValue from '@lib/FunctionValue'
+import { Function } from '@lib/Functions'
+import IValue from '@lib/IValue'
+import { ArgumentsMismatchException, TypeException } from 'exceptions/ArgumentsMismatchException'
+
+export default class StdSort implements Function {
+  public execute(...args: IValue[]): IValue {
+    if (args.length < 1) throw new ArgumentsMismatchException('At least one argument expected')
+    if (!(args[0] instanceof ArrayValue)) throw new TypeException('Array expected in first argument')
+
+    const elements: IValue[] = args[0].getCopyElements()
+    switch (args.length) {
+      case 1:
+        elements.sort()
+        break
+      case 2:
+        if (!(args[1] instanceof FunctionValue)) throw new TypeException('Function expected in second argument')
+        const comparator = args[1].getValue()
+        elements.sort((a, b) => {
+          return comparator.execute(a, b).asNumber()
+        })
+        break
+      default:
+        throw new ArgumentsMismatchException('Wrong number of arguments')
+    }
+
+    return new ArrayValue(elements)
+  }
+}
